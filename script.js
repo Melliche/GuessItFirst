dac8c7fd00173b3bd310f6dc81327b2730b2850c
Julick Mellah <74454432+Melliche@users.noreply.github.com>
2023-11-22 09:59:35 +0100

    on avance

---
+++ script.js
@@ -0,0 +1,52 @@
+let counter = 0;
+function AddInput() {
+  if (counter <= 5) {
+    let input = document.createElement("input");
+    input.type = "text";
+    input.name = "input";
+    input.placeholder = "Name";
+    input.className = "input";
+    input.id = "formId";
+    document.getElementById("inputContainer").appendChild(input);
+    let hr = document.createElement("hr");
+    hr.className = "hr";
+    document.getElementById("inputContainer").appendChild(hr);
+  } else {
+    let alert = document.createElement("p");
+    alert.className = "alert";
+    alert.innerHTML = "You have reached the maximum number of fields";
+    counter <= 6 && document.getElementById("inputContainer").appendChild(alert);
+  }
+  counter++;
+}
+
+function recupererDonnees() {
+  let inputs = document.querySelectorAll('[class^="input"]');
+  let inputValues = [];
+
+  inputs.forEach(function (input) {
+    input.value && inputValues.push(input.value);
+  });
+  if (inputValues.length > 0) {
+    console.log("Contenu des inputs :", inputValues);
+    document.getElementById("formSection").style.display = "none";
+    document.getElementById("resultSection").style.display = "flex";
+    const random = Math.floor(Math.random() * inputValues.length);
+    console.log(random, inputValues[random]);
+    let winner = document.createElement("p");
+    winner.innerHTML = inputValues[random];
+    document.getElementById("winerSpan").innerHTML = winner.innerHTML;
+  } else if (counter <= 1) {
+    let alert = document.createElement("p");
+    alert.className = "alert";
+    alert.innerHTML = "you cannot launch without providing a name";
+    counter <= 6 && document.getElementById("inputContainer").appendChild(alert);
+  }
+  counter++;
+}
+
+function reset() {
+  location.reload();
+}
+
+AddInput();
